2131671511eb18ca70c039751b61788d903ff4be
Uky <ukyaaery+github@gmail.com>
2010-12-13 22:44:06 -0800

    Inventory now managed through an XML spreadsheet file and AJAX webpage so caching shouldn't be an issue.

---
+++ nekostuffs/nekostuffs.js
@@ -0,0 +1,44 @@
+function parseXml(xml) {
+    var working = false;
+    var working_table = '';
+    var table_empty = true;
+    $(xml).find('Row').each(function(){
+        var cells = $(this).children();
+        if(cells.length == 1){
+            if(working && !table_empty) { // close previous table
+                working_table += '</table>';
+                $('#retainer').append(working_table);
+            }
+
+            var category = cells.first().text();
+            table_empty = true;
+            working_table = '<table class="stuffs"><tr><th>' + category + '</th></tr>';
+        }
+        else if(cells.length == 2){
+            var item_name = cells.first().text();
+            var item_url = cells.first().attr('ss:HRef');
+            var quantity = cells.last().text();
+
+            if (quantity != 0 && item_url && item_name) {
+                working_table += '<tr>';
+                working_table += '<td><a href="' + item_url + '">' + item_name + '</a></td>';
+                working_table += '<td class="numeric">' + quantity + '</td>';
+                working_table += '</tr>';
+                table_empty = false;
+            }
+        }
+
+        working = true;
+    });
+    working_table += '</table>';
+    if (!table_empty)
+        $('retainer').append(working_table);
+    working = false;
+
+    YG.Syndication.Regen();
+
+    $('#retainer').masonry({
+        //columnWidth: 200,
+        itemSelector: '.stuffs'
+    });
+}
